b448679d727956e9857121c1165d338a6c00c922
JustDancePC <jesusdavid268@outlook.com>
2021-03-22 01:12:09 -0400

    add helmet

--- myBack/typescript/app.ts
+++ myBack/typescript/app.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
 
 const app = express();
 
 const port = process.env.PORT || 8000;
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
