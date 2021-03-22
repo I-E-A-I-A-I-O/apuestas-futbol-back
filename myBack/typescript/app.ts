c226075afe4e6833659801c3519cf00306b476c1
JustDancePC <jesusdavid268@outlook.com>
2021-03-22 00:18:37 -0400

    a

---
+++ myBack/typescript/app.ts
@@ -0,0 +1,11 @@
+import express, {Request, Response} from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/hola', (req: Request, response: Response) => {
+    response.status(200).json({content: 'jejejejeje'})
+});
