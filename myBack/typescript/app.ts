598e8be24cc494d71f811f5d92a0108a43905112
JustDancePC <jesusdavid268@outlook.com>
2021-03-22 00:55:10 -0400

    Heroku

--- myBack/typescript/app.ts
+++ myBack/typescript/app.ts
@@ -1,11 +1,17 @@
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 
 const app = express();
+
+const port = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
 
 app.post('/hola', (req: Request, response: Response) => {
-    response.status(200).json({content: 'jejejejej'})
+    response.status(200).json({ content: 'jejejejej' })
+});
+
+app.listen(port, () => {
+    console.log(`Running at port ${port}`);
 });
